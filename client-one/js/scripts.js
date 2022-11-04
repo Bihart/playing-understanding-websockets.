d4749488e2294a9285aa87c1e691ee3fbe5d7f00
Bihart <sanket.bihari@gmail.com>
2022-11-04 18:30:44 -0500

    [update] Same as above, but with a ugly graph 🤮.
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- client-one/js/scripts.js
+++ client-one/js/scripts.js
@@ -1,32 +1,38 @@
-'use strict';
-
-// import * as d3 from "https://cdn.skypack.dev/d3@7";
+import * as Plot from "https://cdn.skypack.dev/@observablehq/plot@0.6";
 
 const state = new Map();
 const url = "ws://localhost:8765";
 const my_ws = new WebSocket(url);
 
-my_ws.onmessage = (event) => {
-    const recv_data = event.data;
-    update_state(recv_data);
-    update_view(recv_data);
-};
-
 my_ws.onerror = (event) => {
     console.log(event);
 };
 
-
-function update_state(key_to_update) {
+function update_state({rate: new_value, value: key_to_update }) {
     if(state.has(key_to_update)){
         const prev_value = state.get(key_to_update);
-        state.set(key_to_update, prev_value + 1);
+        state.set(key_to_update, prev_value + new_value);
     } else {
-        state.set(key_to_update, 1);
+        state.set(key_to_update, new_value);
     }
 }
 
-function update_view(key_update) {
-    const curr_target =  document.getElementById(`${key_update}-value`);
-    curr_target.innerText = state.get(key_update);
+function update_all(obj_msg) {
+    update_state(obj_msg);
+    update_view();
+}
+
+my_ws.onmessage = (event) => {
+    const recv_data = JSON.parse(event.data);
+    update_all(recv_data);
+};
+function update_view(){
+    const container = document.querySelector(".graph");
+    const childres = container.children;
+    const data = [...state.entries()].map(([key, value]) => {
+        return {'es': key, 'frec': value};
+    });
+    const chart = Plot.barY(data, {x: 'es', y: 'frec'});
+    const plot = chart.plot();
+    container.innerHTML = plot.outerHTML;
 }
