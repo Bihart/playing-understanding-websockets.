7e6763a1dfb3a1596666e5214e8dc7ab54f99cca
Bihart <sanket.bihari@gmail.com>
2022-11-04 13:55:19 -0500

    [refactor] A little of web standard (?)
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- client-one/js/scripts.js
+++ client-one/js/scripts.js
@@ -1,6 +1,21 @@
+'use strict';
+
+// import * as d3 from "https://cdn.skypack.dev/d3@7";
+
 const state = new Map();
 const url = "ws://localhost:8765";
 const my_ws = new WebSocket(url);
+
+my_ws.onmessage = (event) => {
+    const recv_data = event.data;
+    update_state(recv_data);
+    update_view(recv_data);
+};
+
+my_ws.onerror = (event) => {
+    console.log(event);
+};
+
 
 function update_state(key_to_update) {
     if(state.has(key_to_update)){
@@ -14,9 +29,4 @@ function update_state(key_to_update) {
 function update_view(key_update) {
     const curr_target =  document.getElementById(`${key_update}-value`);
     curr_target.innerText = state.get(key_update);
-}
-my_ws.onmessage = (event) => {
-    const recv_data = event.data;
-    update_state(recv_data);
-    update_view(recv_data);
 }
